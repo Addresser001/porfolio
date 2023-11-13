92636298985207196e2f8c3a947cecf756652369
Addresser001 <sokarithankgod2@gmail.com>
2023-11-12 12:26:30 -1200

    added social media links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -12,10 +12,22 @@ const Footer = () => {
     <div className='footer_wrapper'>
       <span className='connect_container'>
         <span className='small_p'>Connect with me:</span>
-        <a href='#'>{TwitterIcon}</a>
+        <a href='https://twitter.com/iamaddresser_' target='_blank'>
+          {TwitterIcon}
+        </a>
         <a href='#'>{LinkinIcon}</a>
-        <a href='#'>{InstagramIncon}</a>
-        <a href='#'>{GithubIcon}</a>
+        <a
+          href='https://instagram.com/iamaddresser?utm_source=qr&igshid=MzNlNGNkZWQ4Mg=='
+          target='_blank'
+        >
+          {InstagramIncon}
+        </a>
+        <a
+          href='https://github.com/Addresser001?tab=repositories'
+          target='_blank'
+        >
+          {GithubIcon}
+        </a>
       </span>
       <section>
         <span className='x-small_p'>
